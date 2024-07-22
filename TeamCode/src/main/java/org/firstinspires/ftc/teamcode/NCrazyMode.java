14937e075b3081e32e81b1ab3779b5080ccd1b48
Neel Suley <neelsuley@gmail.com>
2024-07-21 17:36:30 -0700

    NCrazyMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NCrazyMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+
+
+public class NCrazyMode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop(){
+        if (gamepad1.a) {
+            telemetry.addData("Right Stick", gamepad1.left_stick_x );
+            telemetry.addData("left stick", gamepad1.right_stick_x);
+
+        }
+        else {
+            telemetry.addData("Right Stick", gamepad1.right_stick_x);
+            telemetry.addData("left stick", gamepad1.left_stick_x);
+        }
+
+    }
+}
+
+
+
