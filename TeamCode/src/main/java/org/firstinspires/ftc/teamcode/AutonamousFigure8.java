57fa14cc894df220e2b4a722a109dda3bd2ca681
Neel Suley <neelsuley@gmail.com>
2024-07-28 18:05:54 -0700

    Figure 8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonamousFigure8.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonamousFigure8.java
@@ -1,19 +1,25 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@Autonomous
 
-@TeleOp()
-
-
-public class AutonamousFigure8 extends OpMode {
+public class AutonamousFigure8 extends LinearOpMode {
     @Override
-    public void init() {
-    @Override
-    public void loop() {
-
-
-
-
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("rearLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("rearRight");
+        frontLeftMotor.setPower(0.8);
+        backLeftMotor.setPower(0.8);
+        frontRightMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
     }
 }
 
