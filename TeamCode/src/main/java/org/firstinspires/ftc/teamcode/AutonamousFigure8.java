cbc70e44b737a26742d64ce1fca7f0df27bd299f
Neel Suley <neelsuley@gmail.com>
2024-07-28 18:18:14 -0700

    Added Runtime

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonamousFigure8.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonamousFigure8.java
@@ -21,10 +21,22 @@ public class AutonamousFigure8 extends LinearOpMode {
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("rearLeft");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("rearRight");
-        frontLeftMotor.setPower(0.8);
-        backLeftMotor.setPower(0.8);
-        frontRightMotor.setPower(0.5);
-        backRightMotor.setPower(0.5);
+
+
+        if (getRuntime() <= 4) {
+            frontLeftMotor.setPower(0.8);
+            backLeftMotor.setPower(0.8);
+            frontRightMotor.setPower(0.5);
+            backRightMotor.setPower(0.5);
+        }
+        if (getRuntime() >=5) {
+            frontLeftMotor.setPower(0.5);
+            backLeftMotor.setPower(0.5);
+            frontRightMotor.setPower(0.8);
+            backRightMotor.setPower(0.8);
+
+
+        }
     }
 }
 
