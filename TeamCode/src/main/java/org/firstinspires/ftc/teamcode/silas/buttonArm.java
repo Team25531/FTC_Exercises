01c1d51805ad7f990f3bcdd5abebf92d77e1ffa9
Silas Ross <Damanz.r48@gmail.com>
2024-09-29 16:22:47 -0700

    trying to move arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silas/buttonArm.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.silas;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+public class buttonArm {
+
+    DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+    DcMotor backLeftMotor = hardwareMap.dcMotor.get("rearLeft");
+    DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+    DcMotor backRightMotor = hardwareMap.dcMotor.get("rearRight");
+
+    if (gamepad1.dpad_down);
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silas/finaldraft.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.silas;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "final draft", group = "TeleOp")
+public class finaldraft extends LinearOpMode {
+
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+    private DcMotor topfrontMotor;
+
+    @Override
+    public void runOpMode() {
+
+        frontLeftMotor = hardwareMap.dcMotor.get( "frontLeft");
+        backLeftMotor = hardwareMap.dcMotor.get( "rearLeft");
+        frontRightMotor = hardwareMap.dcMotor.get( "frontRight");
+        backRightMotor = hardwareMap.dcMotor.get( "frontLeft");
+        topfrontMotor = hardwareMap.dcMotor.get("topfront");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        topfrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        if (gamepad1.dpad_down){
+            topfrontMotor.setPower(0.1);
+            }
+
+        }
+    }
