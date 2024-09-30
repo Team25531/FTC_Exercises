b7e51eb8b5a457e5c3a46a00d8a4b6eba4251b2b
Neel Suley <neelsuley@gmail.com>
2024-09-29 18:20:49 -0700

    Trying to move servo and arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/motorMovementButton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/motorMovementButton.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Neel: Button movement", group = "Neel")
@@ -18,6 +19,8 @@ public class motorMovementButton extends LinearOpMode {
     private DcMotor backRightMotor;// = hardwareMap.dcMotor.get("backRight");
     private DcMotor elbow;// = hardwareMap.dcMotor.get("backRight");
 
+    private Servo finger;
+
 
 
 
@@ -29,23 +32,40 @@ public class motorMovementButton extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
         DcMotor elbow = hardwareMap.dcMotor.get("elbow");
-
+        finger = hardwareMap.get(Servo.class, "finger");
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
         elbow.setDirection(DcMotor.Direction.REVERSE);
-
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
-        // semicircle
         runtime.reset();
-        if (gamepad1.dpad_up) {
-            elbow.setPower(0.1);
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up) {
+                elbow.setPower(0.8);
+            }
+            else if (gamepad1.dpad_down) {
+                elbow.setPower(-0.8);
+                //going down
+            }
+            else {
+                elbow.setPower(0);
 
-
+            }
+            while (gamepad1.dpad_left){
+                finger.setPosition(2);
+            }
+            while (gamepad1.dpad_right){
+                finger.setPosition(-2);
+            }
 
         }
-        }
+    }
 
     }
 
