d5665675889ece1f28b22a45f48f524cf46194bb
Neel Suley <neelsuley@gmail.com>
2024-10-06 17:11:28 -0700

    movement auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/motorMovementButton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/motorMovementButton.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.neel;
 
+import com.google.firestore.v1.Write;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -18,10 +20,10 @@ public class motorMovementButton extends LinearOpMode {
     private DcMotor frontRightMotor;// = hardwareMap.dcMotor.get("frontRight");
     private DcMotor backRightMotor;// = hardwareMap.dcMotor.get("backRight");
     private DcMotor elbow;// = hardwareMap.dcMotor.get("backRight");
-    private Servo finger;
+    private CRServo finger;
 
+    private Servo wrist;
     int position = 0;
-
 
 
     @Override
@@ -32,12 +34,15 @@ public class motorMovementButton extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
         DcMotor elbow = hardwareMap.dcMotor.get("elbow");
-        finger = hardwareMap.get(Servo.class, "finger");
+        finger = hardwareMap.crservo.get("finger");
+        wrist = hardwareMap.servo.get("wrist");
         //setting direction for motors
+
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
         elbow.setDirection(DcMotor.Direction.REVERSE);
         //make it so that motors don't fall automaticly
         elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -45,41 +50,69 @@ public class motorMovementButton extends LinearOpMode {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
         //waiting for start
         waitForStart();
+
+
         //reseting variable called runtime
-        runtime.reset();
-        //code for moving the arm with while and if else code.
+        if (isStopRequested()) return;
+
         while (opModeIsActive()) {
-            if (gamepad1.dpad_up) {
-                elbow.setPower(0.8);
-                //makes arm go up
-            }
-            else if (gamepad1.dpad_down) {
-                elbow.setPower(-0.8);
-                //makes arm go down
-            }
-            else {
-                elbow.setPower(0);
-                //the arm doesn't move
-            }
-            //code for moving the finger aka the rubber tire with while loops
-            while (gamepad1.dpad_left){
-                position = 10;
-                finger.setPosition(position);
-                //tire moves inward to pull block in
-            }
-            while (gamepad1.dpad_right){
-                position = -10;
-            //tire moves outward to push block out
-                finger.setPosition(position);
-            }
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+            double position_target = 0.5;
+            double position_last_set = wrist.getPosition();
+
+
+            runtime.reset();
+
+
+                if (gamepad1.dpad_up) {
+                    elbow.setPower(0.8);
+                    //makes arm go up
+                } else if (gamepad1.dpad_down) {
+                    elbow.setPower(-0.8);
+                    //makes arm go down
+                } else {
+                    elbow.setPower(0);
+                    //the arm doesn't move
+                }
+                //code for moving the finger aka the rubber tire with while loops
+                if (gamepad1.dpad_left) {
+
+                    finger.setPower(0.8);
+                    //tire moves inward to pull block in
+                } else if (gamepad1.dpad_right) {
+
+                    //tire moves outward to push block out
+                    finger.setPower(-0.8);
+                } else {
+                    finger.setPower(0);
+                }
+
 
 
         }
-    }
 
     }
+}
 
 
 
