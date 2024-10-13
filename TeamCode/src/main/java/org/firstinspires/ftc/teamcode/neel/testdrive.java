f3d1e9beb2e3edbb88630928a499b7b96a4bd653
Neel Suley <neelsuley@gmail.com>
2024-10-13 16:01:55 -0700

    Test Gobuilda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/testdrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/testdrive.java
@@ -1,18 +1,23 @@
 package org.firstinspires.ftc.teamcode.neel;
 
+
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
 @TeleOp(name = "Neel: Button movement", group = "Neel")
 //we need to add the DcMotors
-public class motorMovementButton extends LinearOpMode {
+public class testdrive extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftMotor;// = hardwareMap.dcMotor.get("frontLeft");
     private DcMotor backLeftMotor;// = hardwareMap.dcMotor.get("backLeft");
@@ -79,13 +84,12 @@ public class motorMovementButton extends LinearOpMode {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        ((DcMotorEx) elbow).setCurrentAlert(5, CurrentUnit.AMPS);
+        finger.setPower(INTAKE_OFF);
+        wrist.setPosition(WRIST_FOLDED_IN);
 
         //waiting for start
         waitForStart();
-        elbow.setTargetPosition(0);
-        elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
         //reseting variable called runtime
@@ -109,35 +113,109 @@ public class motorMovementButton extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
-            position = 1;
 
 
 
             runtime.reset();
-                wrist.setPosition(position);
 
-                if (gamepad1.dpad_up) {
-                    elbow.setPower(0.8);
-                    //makes arm go up
-                } else if (gamepad1.dpad_down) {
-                    elbow.setPower(-0.8);
-                    //makes arm go down
-                } else {
-                    elbow.setPower(0);
-                    //the arm doesn't move
-                }
-                //code for moving the finger aka the rubber tire with while loops
-                if (gamepad1.dpad_left) {
 
-                    finger.setPower(0.8);
-                    //tire moves inward to pull block in
-                } else if (gamepad1.dpad_right) {
+            if (gamepad1.a) {
+                finger.setPower(INTAKE_COLLECT);
+            }
+            else if (gamepad1.x) {
+                finger.setPower(INTAKE_OFF);
+            }
+            else if (gamepad1.b) {
+                finger.setPower(INTAKE_DEPOSIT);
+            }
+            if(gamepad1.right_bumper){
+                /* This is the intaking/collecting arm position */
+                armPosition = ARM_COLLECT;
+                wrist.setPosition(WRIST_FOLDED_OUT);
+                finger.setPower(INTAKE_COLLECT);
+            }
 
-                    //tire moves outward to push block out
-                    finger.setPower(-0.8);
-                } else {
-                    finger.setPower(0);
-                }
+            else if (gamepad1.left_bumper){
+                    /* This is about 20° up from the collecting position to clear the barrier
+                    Note here that we don't set the wrist position or the intake power when we
+                    select this "mode", this means that the intake and wrist will continue what
+                    they were doing before we clicked left bumper. */
+                armPosition = ARM_CLEAR_BARRIER;
+            }
+
+            else if (gamepad1.y){
+                /* This is the correct height to score the sample in the LOW BASKET */
+                armPosition = ARM_SCORE_SAMPLE_IN_LOW;
+            }
+
+            else if (gamepad1.dpad_left) {
+                    /* This turns off the intake, folds in the wrist, and moves the arm
+                    back to folded inside the robot. This is also the starting configuration */
+                armPosition = ARM_COLLAPSED_INTO_ROBOT;
+                finger.setPower(INTAKE_OFF);
+                wrist.setPosition(WRIST_FOLDED_IN);
+            }
+
+            else if (gamepad1.dpad_right){
+                /* This is the correct height to score SPECIMEN on the HIGH CHAMBER */
+                armPosition = ARM_SCORE_SPECIMEN;
+                wrist.setPosition(WRIST_FOLDED_IN);
+            }
+
+            else if (gamepad1.dpad_up){
+                /* This sets the arm to vertical to hook onto the LOW RUNG for hanging */
+                armPosition = ARM_ATTACH_HANGING_HOOK;
+                finger.setPower(INTAKE_OFF);
+                wrist.setPosition(WRIST_FOLDED_IN);
+            }
+
+            else if (gamepad1.dpad_down){
+                /* this moves the arm down to lift the robot up once it has been hooked */
+                armPosition = ARM_WINCH_ROBOT;
+                finger.setPower(INTAKE_OFF);
+                wrist.setPosition(WRIST_FOLDED_IN);
+            }
+            armPositionFudgeFactor = FUDGE_FACTOR * (gamepad1.right_trigger + (-gamepad1.left_trigger));
+
+
+            /* Here we set the target position of our arm to match the variable that was selected
+            by the driver.
+            We also set the target velocity (speed) the motor runs at, and use setMode to run it.*/
+            elbow.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
+
+            ((DcMotorEx) elbow).setVelocity(2100);
+            elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            /* TECH TIP: Encoders, integers, and doubles
+            Encoders report when the motor has moved a specified angle. They send out pulses which
+            only occur at specific intervals (see our ARM_TICKS_PER_DEGREE). This means that the
+            position our arm is currently at can be expressed as a whole number of encoder "ticks".
+            The encoder will never report a partial number of ticks. So we can store the position in
+            an integer (or int).
+            A lot of the variables we use in FTC are doubles. These can capture fractions of whole
+            numbers. Which is great when we want our arm to move to 122.5°, or we want to set our
+            servo power to 0.5.
+
+            setTargetPosition is expecting a number of encoder ticks to drive to. Since encoder
+            ticks are always whole numbers, it expects an int. But we want to think about our
+            arm position in degrees. And we'd like to be able to set it to fractions of a degree.
+            So we make our arm positions Doubles. This allows us to precisely multiply together
+            armPosition and our armPositionFudgeFactor. But once we're done multiplying these
+            variables. We can decide which exact encoder tick we want our motor to go to. We do
+            this by "typecasting" our double, into an int. This takes our fractional double and
+            rounds it to the nearest whole number.
+            */
+
+            /* Check to see if our arm is over the current limit, and report via telemetry. */
+            if (((DcMotorEx) elbow).isOverCurrent()){
+                telemetry.addLine("MOTOR EXCEEDED CURRENT LIMIT!");
+            }
+
+
+            /* send telemetry to the driver of the arm's current position and target position */
+            telemetry.addData("armTarget: ",elbow.getTargetPosition());
+            telemetry.addData("arm Encoder: ", elbow.getCurrentPosition());
+            telemetry.update();
 
 
 
@@ -145,6 +223,7 @@ public class motorMovementButton extends LinearOpMode {
 
     }
 }
+
 
 
 
