dc7bf570951e6a96e6f142d1c06dccf3e201af93
Aaron Heady <aaron.heady@live.com>
2024-10-19 17:28:44 -0700

    Fixing some files that were breaking the build.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silas/hardwork.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silas/hardwork.java
@@ -1,24 +1,25 @@
-package org.firstinspires.ftc.teamcode.silas;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-public class hardwork {
-
-    private DcMotor frontLeftMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backRightMotor;
-
-    public void runOpMode() {
-
-        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
-        backLeftMotor = hardwareMap.dcMotor.get("rearLeft");
-        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
-        backRightMotor = hardwareMap.dcMotor.get("rearRight");
-
-        if (gamepad1.dpad_down) {
-
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode.silas;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//public class hardwork extends LinearOpMode {
+//
+//    private DcMotor frontLeftMotor;
+//    private DcMotor backLeftMotor;
+//    private DcMotor frontRightMotor;
+//    private DcMotor backRightMotor;
+//
+//    public void runOpMode() {
+//
+//        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+//        backLeftMotor = hardwareMap.dcMotor.get("rearLeft");
+//        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+//        backRightMotor = hardwareMap.dcMotor.get("rearRight");
+//
+//        if (gamepad1.dpad_down) {
+//
+//        }
+//    }
+//}
