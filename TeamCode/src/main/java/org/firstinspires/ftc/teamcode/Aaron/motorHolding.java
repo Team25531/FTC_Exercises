81a85f465a020aad81edd942f14d3cb2276b1158
Aaron Heady <aaron.heady@live.com>
2024-10-23 18:13:08 -0700

    Example class for holding the arm in place.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Aaron/motorHolding.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.Aaron;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "motorHolding", group = "Aaron")
+//@Disabled
+public class motorHolding extends LinearOpMode {
+    int HoldPosition;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor elbow = hardwareMap.dcMotor.get("elbow");
+        elbow.setTargetPosition(0);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (isStopRequested()) return;
+
+            telemetry.addData("Currently goal", HoldPosition);
+            telemetry.addData("Currently   at", elbow.getCurrentPosition());
+
+            if (gamepad1.dpad_up) {
+                elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                elbow.setPower(-0.8);
+                HoldPosition = elbow.getCurrentPosition();
+            } else if (gamepad1.dpad_down) {
+                elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                elbow.setPower(0.8);
+                HoldPosition = elbow.getCurrentPosition();
+            } else {
+                elbow.setPower(0);
+                HoldArmStill(HoldPosition, elbow);
+            }
+            telemetry.update();
+        }
+    }
+
+    public void HoldArmStill(int posToHold, DcMotor motor) {
+        int tolerance = 10;
+        int currentPosition = motor.getCurrentPosition();
+        if ((Math.abs(currentPosition - posToHold)) < tolerance) {
+            return;
+        }
+        motor.setTargetPosition(posToHold);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(Math.abs(0.8));
+    }
+}
