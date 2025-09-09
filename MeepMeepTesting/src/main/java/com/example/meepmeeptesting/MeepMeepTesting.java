a2d25c2952540d831c7f1e19d27e72e24273409d
Aaron <me@aaronheady.com>
2025-09-09 15:04:53 -0700

    Adding some meepmeep test files

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,13 +1,28 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import javax.swing.Box;
+
+//This is from the MeepMeep repository
+//https://github.com/rh-robotics/MeepMeep/tree/master
+
+
 public class MeepMeepTesting {
+
     public static void main(String[] args) {
+
+        TwoLeftsAndSpline();
+       // BoxPattern();
+    }
+
+    public static void BoxPattern() {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -32,6 +47,41 @@ public class MeepMeepTesting {
                 .start();
     }
 
-    
+    public static void TwoLeftsAndSpline() {
+
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .addDisplacementMarker(() -> {
+                                    /* Everything in the marker callback should be commented out */
+
+                                    // bot.shooter.shoot()
+                                    // bot.wobbleArm.lower()
+                                })
+                                .turn(Math.toRadians(90))
+                                .splineTo(new Vector2d(10, 15), 0)
+                                .lineToLinearHeading(new Pose2d(20,20))
+                                .turn(Math.toRadians(90))
+                                .build()
+                );
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_BLACK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+
 
 }
