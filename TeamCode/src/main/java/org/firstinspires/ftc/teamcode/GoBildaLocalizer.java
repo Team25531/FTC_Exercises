15e656c6604266497c25a0dfc78b3a11228fa70c
Aaron <me@aaronheady.com>
2025-09-09 13:32:20 -0700

    Initial install of RoadRunner.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaLocalizer.java
@@ -0,0 +1,72 @@
+//package org.firstinspires.ftc.teamcode;
+//
+//
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//
+//import com.acmerobotics.roadrunner.geometry.Pose2d;
+//import com.acmerobotics.roadrunner.localization.Localizer;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+//import org.firstinspires.ftc.teamcode.lib.GeomUtil;
+//import org.firstinspires.ftc.teamcode.lib.gobilda.GoBildaPinpointDriver;
+//import org.firstinspires.ftc.teamcode.subsystems.drivetrain.DriveConstants;
+//
+//import lombok.experimental.ExtensionMethod;
+//
+//@ExtensionMethod({GeomUtil.class})
+//public class GoBildaLocalizer implements Localizer {
+//    private final GoBildaPinpointDriver odometry;
+//
+//    public GoBildaLocalizer(final HardwareMap hardwareMap, final Pose2d mountOffsets) {
+//        odometry = hardwareMap.get(GoBildaPinpointDriver.class, "od");
+//        odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+//        odometry.setEncoderResolution((DriveConstants.currentRobot == DriveConstants.RobotType.ALPHA)?GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD:GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+////        odometry.setOffsets(mountOffsets.getX(),mountOffsets.getY());
+//        odometry.resetPosAndIMU();
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Pose2d getPoseEstimate() {
+//        odometry.update();
+//        return odometry.getPosition().toPose2d();
+//    }
+//
+//    public double getHeading() {
+//        odometry.update();
+//        return odometry.getHeading();
+//    }
+//
+//    @Override
+//    public void setPoseEstimate(@NonNull Pose2d pose2d) {
+//        odometry.setPosition(pose2d.toPose2D());
+//    }
+//
+//    @Nullable
+//    @Override
+//    public Pose2d getPoseVelocity() {
+//        odometry.update();
+//        return odometry.getVelocity().toPose2d();
+//    }
+//
+//    public double getHeadingVelocity() {
+//        odometry.update();
+//        return odometry.getHeadingVelocity();
+//    }
+//
+//    /**
+//     * IMPORTANT: Requires *stationary* robot.
+//     */
+//    public void recalibrateIMU(){
+//        odometry.recalibrateIMU();
+//    }
+//
+//    @Override
+//    public void update() {}
+//
+//}
