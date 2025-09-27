e17117fb2e434fee6d076cee1ddb0682433b163b
neelsky <neelsuley@gmail.com>
2025-09-27 15:57:59 -0700

    temp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDecodeComp/FtcCompTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDecodeComp/FtcCompTeleop.java
@@ -13,21 +13,85 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class FtcCompTeleop extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftMotor;// = hardwareMap.dcMotor.get("frontLeft");
+    private DcMotor backLeftMotor;// = hardwareMap.dcMotor.get("backLeft");
+    private DcMotor frontRightMotor;// = hardwareMap.dcMotor.get("frontRight");
+    private DcMotor backRightMotor;// = hardwareMap.dcMotor.get("backRight");
 
-    private DcMotor test;
-    double motorTestDirection = 1;
 
-    boolean motorON = false;
+    double position = 0.312;
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1 / 360.0; // we want ticks per degree, not per rotation
 
-    boolean motorDirection = false;
+
+    final double ARM_COLLAPSED_INTO_ROBOT = 0;
+    final double ARM_COLLECT = 250 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER = 230 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK = 120 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
+
+    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
+    final double INTAKE_COLLECT = -1.0;
+    final double INTAKE_OFF = 0.0;
+    final double INTAKE_DEPOSIT = 0.5;
+
+    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
+    final double WRIST_FOLDED_IN = 0.8333;
+    final double WRIST_FOLDED_OUT = 0.5;
+
+    /* A number in degrees that the triggers can adjust the arm position by */
+    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
+
+    /* Variables that are used to set the arm to a specific position */
+    double armPosition = (int) ARM_COLLAPSED_INTO_ROBOT;
+    double armPositionFudgeFactor;
+    int HoldPosition;
 
 
     @Override
-
     public void runOpMode() throws InterruptedException {
-        waitForStart();
 
-        DcMotor test = hardwareMap.dcMotor.get("test");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
+
+
+        //setting direction for motors
+
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+
+        position = 0;//remove
+        double addposition = 0.001;
+        double addedcurrentPosition = position;
+        double subposition = -0.001;
+        double subcurrentPosition = position;
+        int armPosition = 0;
+        double armMovePos = 0.001;
+        boolean gravityCondition = true;
+
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        //waiting for start
+        waitForStart();
+        //  elbow.setTargetPosition(0);
+        //  elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //  elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
         //reseting variable called runtime
@@ -41,43 +105,29 @@ public class FtcCompTeleop extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
+            double speedModifier = Math.abs(1.5);
+            denominator *= speedModifier;
+
+            if (gamepad1.y) {
+                denominator *= 2.5;
+            }
+
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
 
 
             runtime.reset();
 
-
-            if (gamepad1.y) {
-
-                test.setPower(-1 * motorTestDirection);
-
-                //debug
-                telemetry.addData("y", 0);
-                telemetry.update();
-
-                motorON = true;
-                telemetry.addData("Motor on",0);
-
-
-
-                //makes arm go up
-            }
-            if (gamepad1.x){
-                test.setPower(-0.8 * motorTestDirection);
-
-            }
-            if (gamepad1.a){
-                test.setPower(-0.6 * motorTestDirection);
-
-            }
-            if (gamepad1.b){
-                test.setPower(-0.4 * motorTestDirection);
-
-            }
-
         }
-
-
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/NeelTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/NeelTeleop.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.neel;
 
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -10,125 +14,82 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "NeelTeleop", group = "Ftc Comp")
+
 //we need to add the DcMotors
-public class NeelTeleop extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeftMotor;// = hardwareMap.dcMotor.get("frontLeft");
-    private DcMotor backLeftMotor;// = hardwareMap.dcMotor.get("backLeft");
-    private DcMotor frontRightMotor;// = hardwareMap.dcMotor.get("frontRight");
-    private DcMotor backRightMotor;// = hardwareMap.dcMotor.get("backRight");
+public void init() {
+    telemetry.addData("Status", "Initialized");
 
+    // Initialize the hardware variables. Note that the strings used here as parameters
+    // to 'get' must correspond to the names assigned during the robot configuration
+    // step (using the FTC Robot Controller app on the phone).
+    leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+    rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-    double position = 0.312;
-    final double ARM_TICKS_PER_DEGREE =
-            28 // number of encoder ticks per rotation of the bare motor
-                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
-                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
-                    * 1 / 360.0; // we want ticks per degree, not per rotation
+    // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+    // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+    // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+    leftDrive.setDirection(DcMotor.Direction.REVERSE);
+    rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-
-    final double ARM_COLLAPSED_INTO_ROBOT = 0;
-    final double ARM_COLLECT = 250 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER = 230 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK = 120 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
-
-    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
-    final double INTAKE_COLLECT = -1.0;
-    final double INTAKE_OFF = 0.0;
-    final double INTAKE_DEPOSIT = 0.5;
-
-    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
-    final double WRIST_FOLDED_IN = 0.8333;
-    final double WRIST_FOLDED_OUT = 0.5;
-
-    /* A number in degrees that the triggers can adjust the arm position by */
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
-
-    /* Variables that are used to set the arm to a specific position */
-    double armPosition = (int) ARM_COLLAPSED_INTO_ROBOT;
-    double armPositionFudgeFactor;
-    int HoldPosition;
-
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
-
-
-        //setting direction for motors
-
-
-        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
-
-
-        position = 0;//remove
-        double addposition = 0.001;
-        double addedcurrentPosition = position;
-        double subposition = -0.001;
-        double subcurrentPosition = position;
-        int armPosition = 0;
-        double armMovePos = 0.001;
-        boolean gravityCondition = true;
-
-
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-        //waiting for start
-        waitForStart();
-        //  elbow.setTargetPosition(0);
-        //  elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //  elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
-        //reseting variable called runtime
-        if (isStopRequested()) return;
-
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
-            double speedModifier = Math.abs(1.5);
-            denominator *= speedModifier;
-
-            if (gamepad1.y) {
-                denominator *= 2.5;
-            }
-
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-
-            runtime.reset();
-
-        }
-    }
+    // Tell the driver that initialization is complete.
+    telemetry.addData("Status", "Initialized");
 }
+
+/*
+ * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+ */
+@Override
+public void init_loop() {
+}
+
+/*
+ * Code to run ONCE when the driver hits START
+ */
+@Override
+public void start() {
+    runtime.reset();
+}
+
+/*
+ * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+ */
+@Override
+public void loop() {
+    // Setup a variable for each drive wheel to save power level for telemetry
+    double leftPower;
+    double rightPower;
+
+    // Choose to drive using either Tank Mode, or POV Mode
+    // Comment out the method that's not used.  The default below is POV.
+
+    // POV Mode uses left stick to go forward, and right stick to turn.
+    // - This uses basic math to combine motions and is easier to drive straight.
+    double drive = -gamepad1.left_stick_y;
+    double turn  =  gamepad1.right_stick_x;
+    leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+    rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+    // Tank Mode uses one stick to control each wheel.
+    // - This requires no math, but it is hard to drive forward slowly and keep straight.
+    // leftPower  = -gamepad1.left_stick_y ;
+    // rightPower = -gamepad1.right_stick_y ;
+
+    // Send calculated power to wheels
+    leftDrive.setPower(leftPower);
+    rightDrive.setPower(rightPower);
+
+    // Show the elapsed game time and wheel power.
+    telemetry.addData("Status", "Run Time: " + runtime.toString());
+    telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+}
+
+/*
+ * Code to run ONCE after the driver hits STOP
+ */
+@Override
+public void stop() {
+}
+
