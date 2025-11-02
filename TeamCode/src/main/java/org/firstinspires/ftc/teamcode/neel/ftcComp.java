52f4193590b00242bf81dab64d8ab28014597f0e
neelsky <neelsuley@gmail.com>
2025-11-01 20:22:56 -0700

    I added more data points they are in comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/ftcComp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/ftcComp.java
@@ -1,224 +1,324 @@
 package org.firstinspires.ftc.teamcode.neel;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import android.util.Size;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "FtcComp", group = "Ftc Comp")
-//we need to add the DcMotors
-@Disabled
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+
+@TeleOp(name = "FtcCompRed", group = "RedFtcComp")
 public class ftcComp extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeftMotor;// = hardwareMap.dcMotor.get("frontLeft");
-    private DcMotor backLeftMotor;// = hardwareMap.dcMotor.get("backLeft");
-    private DcMotor frontRightMotor;// = hardwareMap.dcMotor.get("frontRight");
-    private DcMotor backRightMotor;// = hardwareMap.dcMotor.get("backRight");
-    private DcMotor elbow;// = hardwareMap.dcMotor.get("backRight");
-    private DcMotor extension;
-    private CRServo finger;
+    private ElapsedTime storageTimer = new ElapsedTime();
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+    private DcMotorEx outtake;
+    private CRServo intake;
+    private CRServo storageWheel;
+    private AprilTagProcessor aprilTag;
+    public static final String WEBCAM_NAME = "Webcam 1";
+    public static final int TARGET_TAG_ID = 24;
+    public static  int IDLE_VELOCITY = 600;
+    private VisionPortal visionPortal;
+    int goalVelocity = 0;
+    double range = 0.05;
+    double distanceToTarget = 0;
+    double angleToTarget = 0;
 
+    boolean isIdleEnabled = false;
 
-    double position = 0.312;
-    final double ARM_TICKS_PER_DEGREE =
-            28 // number of encoder ticks per rotation of the bare motor
-                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
-                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
-                    * 1/360.0; // we want ticks per degree, not per rotation
-
-
-    final double ARM_COLLAPSED_INTO_ROBOT  = 0;
-    final double ARM_COLLECT               = 250 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER         = 230 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN        = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW   = 160 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK   = 120 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT           = 15  * ARM_TICKS_PER_DEGREE;
-
-    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
-    final double INTAKE_COLLECT    = -1.0;
-    final double INTAKE_OFF        =  0.0;
-    final double INTAKE_DEPOSIT    =  0.5;
-
-    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
-    final double WRIST_FOLDED_IN   = 0.8333;
-    final double WRIST_FOLDED_OUT  = 0.5;
-
-    /* A number in degrees that the triggers can adjust the arm position by */
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
-
-    /* Variables that are used to set the arm to a specific position */
-    double armPosition = (int)ARM_COLLAPSED_INTO_ROBOT;
-    double armPositionFudgeFactor;
-    int HoldPosition;
-
-
+    //boolean useOuttake = true;
+    boolean isShooting = false;
+    boolean isAutoAimEnabled = true;
+    boolean isAtGoalVelocity = false;
+    boolean shooterNeedsReset = false;
+    boolean isAimedAtTarget = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
-        DcMotor extension = hardwareMap.dcMotor.get("extension");
+        initializeMotors();
+        initializeTagProcessor();
+        SetIdleState();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (isStopRequested()) return;
+
+            checkIfShooting();
+            double ve;
+
+                ve = (int) ((0.0006 * Math.pow(distanceToTarget, 2)) + (4.8385 * distanceToTarget) + 721.11);
+                telemetry.addData("current velocity", ve);
+
+            doDriving();
+            autoAimOnOff();
+            controlIntake();
+            checkToResetState();
+            checkSetIdleState();
+            setGoalVelocity();
+            runOuttakeMotor();
+            doShooting();
+
+            telemetry.update();
+        }
+    }
+
+    private void doShooting() {
+        if (!isShooting) return;
 
 
-        DcMotor elbow = hardwareMap.dcMotor.get("elbow");
-        finger = hardwareMap.crservo.get("finger");
+        //if we're using auto aim and it isn't yet at the target then let steering keep going.
+        if (isAutoAimEnabled && !isAimedAtTarget) return;
 
-        //setting direction for motors
+        //if we are at goal, then run the storageWheel.
+        if (isAtGoalVelocity && !shooterNeedsReset) {
+            storageTimer.reset();
+            storageWheel.setPower(-1);
+            //todo: determine correct duration for this timer.
+            while (opModeIsActive() & storageTimer.milliseconds() < 3000 && !shooterNeedsReset) {
+                sleep(1);
+                //todo: delete this telemetry.
+                telemetry.addData("storeTimer", storageTimer.milliseconds());
+                telemetry.update();
+            }
+            shooterNeedsReset = true;
+            SetIdleState();
+        }
+        telemetry.addData("shooterNeedsReset", shooterNeedsReset);
+    }
 
+    private void setGoalVelocity() {
+        //only compute velocity if we're actually shooting.
+        if (isShooting && !shooterNeedsReset) {
+            //todo: set these range values
+            if (distanceToTarget > 55 && distanceToTarget < 129) {
+//               int targetY = 1050;
+//                int targetX = 1025;
+//                int targetZ = 1000;
+//                int targetA = 1350;
+//                if (distanceToTarget == 125 ) {
+//                    goalVelocity = 1350;
+//                }
+//                if (distanceToTarget == 70 ) {
+//                    goalVelocity = 1095;
+//                }
+//                if (distanceToTarget == 65) {
+//                    goalVelocity = 1080;
+//                }
+//                if (distanceToTarget == 60) {
+//                    goalVelocity = 1040;             }
+                goalVelocity = (int) ((0.0006 * Math.pow(distanceToTarget, 2)) + (4.8385 * distanceToTarget) + 721.11);
+                telemetry.addData("SHOOT, curDist", distanceToTarget);
+            } else {
+                telemetry.addData("NO SHOOT, curDist", distanceToTarget);
+            }
+        } else {
+            SetIdleState();
+        }
+        telemetry.addData("goalVelocity", goalVelocity);
+    }
+
+    private void checkSetIdleState() {
+        if (gamepad1.leftBumperWasPressed()) {
+             if(IDLE_VELOCITY == 600){
+                 IDLE_VELOCITY = 0;
+                 return;
+             }
+             if(IDLE_VELOCITY == 0){
+                 IDLE_VELOCITY = 600;
+                 return;
+                 //61 1018
+             }
+
+        }
+    }
+    //DATA POINTS //61 1018 //79 1107//65 1080//60 1040
+    private void checkToResetState() {
+        //Try to stop anything that isn't responding to other commands.
+        if (gamepad1.right_bumper) {
+            goalVelocity = 0;//but Idle is going to get reset after this.
+            outtake.setPower(0);
+            intake.setPower(0);
+            storageWheel.setPower(0);
+        }
+    }
+
+    private void controlIntake() {
+        //Bring artifacts into the bot queue
+        if (gamepad1.dpad_down) {
+            intake.setPower(-1);
+        }
+        //Push artifacts out of the bot queue
+        if (gamepad1.dpad_up) {
+            intake.setPower(1);
+        }
+        //Stop intake with any other dpad or x.
+        if (gamepad1.x || gamepad1.dpad_left || gamepad1.dpad_right) {
+            intake.setPower(0);
+        }
+    }
+
+    private void autoAimOnOff() {
+        //toggle autoAim on off if something is wonky.
+        if (gamepad1.yWasPressed()) {
+            isAutoAimEnabled = !isAutoAimEnabled;
+        }
+        telemetry.addData("isAutoAimEnabled", isAutoAimEnabled);
+    }
+
+    private void checkIfShooting() {
+        //to shoot, hold down the left_trigger.
+        if (gamepad1.left_trigger > 0 && !shooterNeedsReset) {
+            isShooting = true;
+        } else if (gamepad1.left_trigger == 0) {
+            isShooting = false;
+            shooterNeedsReset = false;
+        }
+    }
+
+    private void runOuttakeMotor() {
+        double currentVelocity = outtake.getVelocity();
+        double currentPower = outtake.getPower();
+        telemetry.addData("curVelocity", currentVelocity);
+        telemetry.addData("curPower", currentPower);
+
+        double minRange = goalVelocity - (goalVelocity * range);
+        double maxRange = goalVelocity + (goalVelocity * range);
+
+        if (currentVelocity < minRange) {
+            currentPower = currentPower + 0.001;
+        }
+        if (currentVelocity > maxRange) {
+            currentPower = currentPower - 0.001;
+        }
+        isAtGoalVelocity = (currentVelocity < maxRange && currentVelocity > minRange);
+        telemetry.addData("isAtGoalVelocity", isAtGoalVelocity);
+        if(isAtGoalVelocity){
+            return;
+        }
+
+        outtake.setPower(currentPower);
+    }
+
+    private void SetIdleState() {
+        goalVelocity = IDLE_VELOCITY; //min speed for Outtake wheel
+        storageWheel.setPower(0); //stop the storage wheel
+    }
+
+    private void doDriving() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x * 1.1;
+        double rx = gamepad1.right_stick_x;
+
+        angleToTarget = getAngleToTag(TARGET_TAG_ID);
+        distanceToTarget = getDistanceToTag(TARGET_TAG_ID);
+        telemetry.addData("dist  goal", distanceToTarget);
+        telemetry.addData("angle goal", angleToTarget);
+
+        //if shooting and the camera is working then override driver input
+        //and try to steer towards the target.
+        if (isShooting && isAutoAimEnabled && !shooterNeedsReset) {
+            isAimedAtTarget = !(angleToTarget < -1 || angleToTarget > 1);
+            if (!isAimedAtTarget) {
+                if (angleToTarget < -1) rx = 0.3;
+                if (angleToTarget > 1) rx = -0.3;
+            }
+            telemetry.addData("isAimedAtTarget", isAimedAtTarget);
+        }
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        //todo: enable this.
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+
+    private void initializeMotors() {
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        backRightMotor = hardwareMap.dcMotor.get("backRight");
+        intake = hardwareMap.crservo.get("intake");
+        storageWheel = hardwareMap.crservo.get("storageWheel");
+        outtake = hardwareMap.get(DcMotorEx.class, "outtake");
+        //TODO: fix this to pull the correct object.
+        //outtake = hardwareMap.get(DcMotorEx.class, "frontLeft");
 
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
 
-        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        position = 0;//remove
-        double addposition = 0.001;
-        double addedcurrentPosition = position;
-        double subposition = -0.001;
-        double subcurrentPosition = position;
-        int armPosition = 0;
-        double armMovePos = 0.001;
-        boolean gravityCondition = true;
-
-        elbow.setDirection(DcMotor.Direction.REVERSE);
-        //make it so that motors don't fall automaticly
-        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        extension.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-        //waiting for start
-        waitForStart();
-        //  elbow.setTargetPosition(0);
-        //  elbow.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //  elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
-        //reseting variable called runtime
-        if (isStopRequested()) return;
-
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
-            double speedModifier = Math.abs(1.5);
-            denominator *= speedModifier;
-
-            if (gamepad1.y) {
-                denominator *= 2.5;
-            }
-
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-
-
-
-
-
-
-            runtime.reset();
-
-
-            telemetry.addData("Currently goal", HoldPosition);
-            telemetry.addData("Currently   at", elbow.getCurrentPosition());
-            telemetry.addData("Gravity code is now ", gravityCondition);
-
-            if (gamepad1.right_bumper) {
-                elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                elbow.setPower(-0.9);
-                HoldPosition = elbow.getCurrentPosition();
-            } else if (gamepad1.right_trigger > 0) {
-                elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                elbow.setPower(0.9);
-                telemetry.addData("currently at :", elbow.getCurrentPosition());
-                HoldPosition = elbow.getCurrentPosition();
-            } else {
-                elbow.setPower(0);
-                if (gravityCondition) {
-                    HoldArmStill(HoldPosition, elbow);
-                }
-            }
-
-            telemetry.update();
-
-
-            //code for moving the finger aka the rubber tire with while loops
-            if (gamepad1.left_bumper) {
-
-                finger.setPower(0.8);
-                //tire moves inward to pull block in
-            } else if (gamepad1.left_trigger > 0) {
-                //tire moves outward to push block out
-                finger.setPower(-0.8);
-            } else {
-                finger.setPower(0);
-            }
-
-            if (gamepad1.a) {
-                if (gravityCondition) {
-                    gravityCondition = false;
-                }
-
-            }
-
-            if (gamepad1.dpad_up) {
-                extension.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                extension.setPower(0.9);
-                HoldPosition = extension.getCurrentPosition();
-            }
-            else if (gamepad1.dpad_down){
-                extension.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                extension.setPower(-0.9);
-                HoldPosition = extension.getCurrentPosition();
-            }
-             else {
-                extension.setPower(0);
-                HoldArmStill(HoldPosition, extension);
-            }
-
-        }
-
-    } public void HoldArmStill(int posToHold, DcMotor motor) {
-        int tolerance = 4;
-        int currentPosition = motor.getCurrentPosition();
-        if ((Math.abs(currentPosition - posToHold)) < tolerance) {
-            return;
-        }
-        motor.setTargetPosition(posToHold);
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor.setPower(Math.abs(0.8));
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
-//    public void StopArmMoving ( DcMotor motor){
-//        motor.setTargetPosition(800);
-//        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//
-//    }
+
+    private int getDistanceToTag(int tagID) {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        int range = 0;
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.id == tagID) {
+                range = (int) detection.ftcPose.range;
+                break;
+            }
+        }
+        return range;
+    }
+
+    private int getAngleToTag(int tagID) {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        int angle = 0;
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.id == tagID) {
+                angle = (int) detection.ftcPose.bearing;
+                break;
+            }
+        }
+        return angle;
+    }
+
+    private void initializeTagProcessor() {
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawTagOutline(true)
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, WEBCAM_NAME));
+
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+
+        visionPortal.setProcessorEnabled(aprilTag, true);
+    }
 
 }
