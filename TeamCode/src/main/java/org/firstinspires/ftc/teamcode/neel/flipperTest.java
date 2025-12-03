42d141dd4ec3fda85f2c2b2b80fefe8923390115
neelsky <neelsuley@gmail.com>
2025-12-02 21:01:57 -0800

    THREE BALL STORAGE WORKS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/flipperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neel/flipperTest.java
@@ -36,7 +36,8 @@ public class flipperTest extends LinearOpMode {
     public static double NEW_F = 15.0;
 
     private CRServo intake2;
-
+    private Servo flipper;
+    private DcMotorEx outtake;
     private CRServo intake;
     private CRServo intake3;
 
@@ -75,16 +76,20 @@ public class flipperTest extends LinearOpMode {
 
 
 
-
+            stopAll();
 
 
 
             intake3Movement();
+            double flipperPos = flipper.getPosition();
+            flipperPos = flipper.getPosition();
+            telemetry.addData("flipperPos",flipperPos);
 
+            telemetry.update();
             intake2Movement();
-//            flip();
+            flip();
             controlIntake();
-
+            moveOuttakeMotor();
 
             telemetry.update();
         }
@@ -101,44 +106,69 @@ public class flipperTest extends LinearOpMode {
 
     //DATA POINTS //61 1018 //79 1200//65 1080//60 1040//68 1110// 70 1150// 125 1350
     //NEW DATA ///
+    private void flip(){
+        if(gamepad1.rightStickButtonWasPressed()) {
+            flipper.setPosition(0.5);
 
+        }
+        if(gamepad1.leftStickButtonWasPressed()){
+            flipper.setPosition(0.8);
+        }
+    }
 
 
     private void controlIntake() {
         //Bring artifacts into the bot queue
         if (gamepad1.dpad_down) {
-            intake.setPower(-1);
+            intake.setPower(-0.4);
         }
         //Push artifacts out of the bot queue
         if (gamepad1.dpad_up) {
-            intake.setPower(1);
+            intake.setPower(0.4);
         }
         //Stop intake with any other dpad or x.
-        if (gamepad1.x || gamepad1.dpad_left || gamepad1.dpad_right) {
+        if (gamepad1.x) {
             intake.setPower(0);
         }
     }
 
+    private void moveOuttakeMotor(){
+        if(gamepad1.dpad_left){
+            outtake.setPower(0.9);
+        }
+        if(gamepad1.dpad_right){
+            outtake.setPower(0);
+        }
+    }
 
+    private void stopAll(){
+        if(gamepad1.aWasPressed()) {
+            intake.setPower(0);
+            intake2.setPower(0);
+            intake3.setPower(0);
+        }
+    }
 
 
 
     private void intake2Movement(){
         if(gamepad1.leftBumperWasPressed()){
-            intake2.setPower(-1);
+            intake2.setPower(-0.4);
         }
         if(gamepad1.rightBumperWasPressed()){
-            intake2.setPower(1);
+            intake2.setPower(0.4);
         }
     }
     private void intake3Movement(){
         if(gamepad1.bWasPressed()){
-            intake3.setPower(-1);
+            intake3.setPower(-0.8);
         }
         if(gamepad1.yWasPressed()){
-            intake3.setPower(1);
+            intake3.setPower(0.8);
         }
     }
+
+
 
 
 
@@ -147,8 +177,9 @@ public class flipperTest extends LinearOpMode {
         intake = hardwareMap.crservo.get("intake");
         intake3 = hardwareMap.crservo.get("intake3");
 
-;
+;       flipper = hardwareMap.servo.get("flipper");
         intake2 = hardwareMap.crservo.get("intake2");
+        outtake = hardwareMap.get(DcMotorEx.class, "outtake");
         myControlHubVoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
         //TODO: fix this to pull the correct object.
         //outtake = hardwareMap.get(DcMotorEx.class, "frontLeft");
